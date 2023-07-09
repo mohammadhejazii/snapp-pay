6aa48be84b862af5e283849b096356c089962738
mohammad hejazi <smohammadhejazii@gmail.com>
2023-07-09 09:39:06 +0330

    initialize project with spring initializer

---
+++ src/main/java/ir/snapppay/wallet/SnappPayWalletApplication.java
@@ -0,0 +1,13 @@
+package ir.snapppay.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnappPayWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnappPayWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/snapppay/wallet/SnappPayWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.snapppay.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnappPayWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
